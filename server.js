a266af821b2e17f180592fd7d0777ac1d599a20c
Matt Thorning <matthewthorning@gmail.com>
2018-11-12 21:19:00 +0000

    Tidied up the server file and got rid of the annoying socket.io errors

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const redis = require('redis')
 const session = require('express-session')
 const redisStore = require('connect-redis')(session)
 const app = express()
-const http = require('http').Server(app)
-const io = require('socket.io')(http)
+const server = app.listen(3000)
+const io = require('socket.io').listen(server)
 const bodyParser = require('body-parser')
 const uuid = require('uuid/v4')
 const passport = require('passport')
@@ -21,60 +21,24 @@ client.on('connect', () => {
 client.on('error', err => console.error('Error connecting to DB', err))
 
 passport.use(
-    new LocalStrategy(function(username, password, done) {
-        console.log('Authenticating ', username, password, client.get(username))
-        const userFound = client.get(username, function(err, pass) {
-            console.log('Got ', err, pass)
-            if (err) {
-                return done(err)
-            }
-            if (!pass) {
-                return done(null, false)
-            }
-            if (!verifyPassword(password, pass)) {
-                return done(null, false)
-            }
+    new LocalStrategy((username, password, done) => {
+        const userFound = client.get(username, (err, pass) => {
+            if (err) return done(err)
+
+            if (!pass) return done(null, false)
+
+            if (pass !== password) return done(null, false)
+
             return done(null, username)
         })
         if (userFound === false) return done(null, false)
-
-        function verifyPassword(password, pass) {
-            return pass === password
-        }
     })
 )
 
-// passport.use(
-//     new LocalStrategy(
-//         { usernameField: 'username' },
-//         (username, password, done) => {
-//             console.log('username: %s, password: %s', username, password)
-//             client.get(username, (err, pass) => {
-//                 console.log('pass = ', pass)
-//                 if (err) console.error(err)
+passport.serializeUser((user, done) => done(null, user))
 
-//                 if (pass === password) {
-//                     console.log('Password correct')
-//                     return done(null, username)
-//                 } else {
-//                     console.log('Password (%s) incorrect', password)
-//                     return done(null, false, {
-//                         message: 'Incorrect username.'
-//                     })
-//                 }
-//             })
-//         }
-//     )
-// )
-
-passport.serializeUser((user, done) => {
-    console.log('Inside serializeUser callback, user = ', user)
-    done(null, user)
-})
-
-passport.deserializeUser(function(user, done) {
+passport.deserializeUser((user, done) => {
     client.get(user, (err, foundUser) => {
-        console.log('found user: ', foundUser)
         if (err) return done(null, err)
         done(null, foundUser)
     })
@@ -86,14 +50,7 @@ app.use(bodyParser.json())
 app.use(
     session({
         secret: 'esmaesqishpants',
-        genid: req => {
-            console.log(
-                'Inside the session middleware, session ID',
-                req.sessionID
-            )
-            return uuid() // use UUIDs for session IDs
-        },
-        // create new redis store.
+        genid: req => uuid(),
         store: new redisStore({
             host: 'localhost',
             port: 6379,
@@ -109,33 +66,29 @@ app.use(passport.initialize())
 app.use(passport.session())
 
 app.get('/', require('connect-ensure-login').ensureLoggedIn(), (req, res) => {
-    console.log('DId it work?')
     res.sendFile(path.resolve(__dirname, 'dist', 'chat.html'))
     app.use(express.static(path.resolve(__dirname, 'dist')))
 })
 
-app.get('/login', (req, res) => {
-    console.log('GET login')
+app.get('/login', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'login', 'login.html'))
-})
+)
 
 app.post(
     '/login',
     passport.authenticate('local', { failureRedirect: '/login' }),
-    function(req, res) {
-        res.redirect('/')
-    }
+    (req, res) => res.redirect('/')
 )
 
-app.get('/logout', function(req, res) {
-    console.log('logging out ')
+app.get('/logout', (req, res) => {
     req.logout()
     res.redirect('/')
 })
 
 app.get('/whoami', (req, res) => {
-    console.log('Requesting whoami')
-    res.status(200).send({ data: req.session })
+    const user =
+        req.session && req.session.passport && req.session.passport.user
+    res.status(200).send({ user })
 })
 
 io.on('connection', socket => {
@@ -144,8 +97,4 @@ io.on('connection', socket => {
         io.emit('chat message', { ...msg, user })
     })
     socket.on('disconnect', () => console.log('Socket disconnected'))
-})
-
-http.listen(3000, function() {
-    console.log('listening on port:3000')
 })
