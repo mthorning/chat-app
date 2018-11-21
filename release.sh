8692cb3377d0307d2814b10bbfba2c869486b3a2
Matt Thorning <matthewthorning@gmail.com>
2018-11-21 21:43:46 +0000

    tidy-up commit

--- release.sh
+++ release.sh
@@ -2,10 +2,10 @@ ssh vps <<'ENDSSH'
     echo "removing old container..."
     docker-compose rm -sf
     echo "removing old image..."
-    docker rmi socket-chat_web -f
+    docker image rm squishychat_web -f
     echo "renewing directory for new files..."
-    rm -rf ~/containers/socket-chat
-    mkdir ~/containers/socket-chat
+    rm -rf /containers/squishychat
+    mkdir /containers/squishychat
 ENDSSH
 
 echo "creating tarball..."
@@ -13,12 +13,12 @@ tar cvfz ./release-package.tar.gz ./package.json ./docker-compose.yml \
  ./Dockerfile ./server ./build/dist ./login 
 
 echo "transferring tarball to server..."
-scp ./release-package.tar.gz vps:~/containers/socket-chat \
+scp ./release-package.tar.gz server:/containers/squishychat \
 && rm ./release-package.tar.gz
 
 
-ssh vps <<'ENDSSH'
-    cd ~/containers/socket-chat
+ssh server <<'ENDSSH'
+    cd /containers/squishychat
     echo "unzipping tarball"
     tar -xvzf release-package.tar.gz
     echo "building new image..."
