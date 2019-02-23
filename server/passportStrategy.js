f34e81deb1b10c8b00423acf18126bfc9bfc5aac
Matt Thorning <matthewthorning@gmail.com>
2019-02-23 15:40:22 +0000

    Made persistent message history

--- server/passportStrategy.js
+++ server/passportStrategy.js
@@ -1,52 +1,46 @@
-const LocalStrategy = require('passport-local').Strategy
-const UserModel     = require('./mongo/models/User')
-const hash          = require('password-hash-and-salt')
+const LocalStrategy = require("passport-local").Strategy;
+const UserModel = require("./mongo/models/User");
+const hash = require("password-hash-and-salt");
 
 module.exports = passport => {
     passport.use(
         new LocalStrategy((username, password, done) => {
             UserModel.findOne({ username }, async (err, user) => {
                 if (err) {
-                    return done(err)
+                    return done(err);
                 }
                 if (!user) {
-                    console.info('User does not exist')
                     return done(null, false, {
-                        error: 'Incorrect username or password'
-                    })
+                        error: "Incorrect username or password"
+                    });
                 }
                 hash(password).verifyAgainst(user.password, (err, verified) => {
-                    console.log('verifying password')
-                    if(err) console.error(err)
-                    if(!verified) {
-                        console.log('Passwords do not match')
+                    if (err) console.error(err);
+                    if (!verified) {
                         return done(null, false, {
-                            error: 'Incorrect username or password'
-                        })
+                            error: "Incorrect username or password"
+                        });
                     } else {
-                        console.log('password verified', user)
-                        return done(null, user)
+                        return done(null, user);
                     }
-                })
-            })
+                });
+            });
         })
-    )
+    );
 
     //serialize returns the _id property from LoginsModel record
-    passport.serializeUser((user, done) => done(null, user.id))
+    passport.serializeUser((user, done) => done(null, user.id));
 
     //deserialize finds the UsersModel record which matches _id
     //from above
     passport.deserializeUser((id, done) => {
-        console.log('passport deserializing...')
         //This data will be attached to all requests at
         //req.session.passport.user
         UserModel.findById(id, (err, user) => {
-            console.log('strategy deserialize', user)
-            if(err) done(err)
-            done(null, user)
-        })
-    })
+            if (err) done(err);
+            done(null, user);
+        });
+    });
 
-    return passport
-}
+    return passport;
+};
--- server/socket.js
+++ server/socket.js
@@ -1,49 +1,60 @@
-const UserModel   = require('./mongo/models/User')
+const UserModel = require("./mongo/models/User");
+const MessageModel = require("./mongo/models/Message");
 
 module.exports = io => socket => {
+    let userId;
 
-    let userId
+    socket.on("user connected", packet => {
+        sendMessageHistory();
+        userId = packet.id;
+        UserModel.findByIdAndUpdate(userId, { $set: { online: true } }, err => {
+            if (err) console.error(err);
+            sendOnlineUsers();
+        });
+    });
 
-    socket.on('user connected', packet => {
-        userId = packet.id
-        console.log('user id', userId)
-        UserModel.findByIdAndUpdate(
-            userId,
-            { $set: { online: true } },
-            err => {
-                if(err) console.error(err)
-                sendOnlineUsers()
-            }
-        )
-    })
+    socket.on("chat message", packet => {
+        console.log(packet);
+        saveMessage(packet);
+        let responsePacket = { ...packet };
+        io.emit("chat message", responsePacket);
+    });
 
-    socket.on('chat message', packet => {
-        let responsePacket = { ...packet }
-        io.emit('chat message', responsePacket)
-    })
+    socket.on("adult message", packet => {
+        let responsePacket = { ...packet };
+        io.emit("adult message", responsePacket);
+    });
 
-    socket.on('adult message', packet => {
-        let responsePacket = { ...packet }
-        io.emit('adult message', responsePacket)
-    })
-
-    socket.on('disconnect', () => {
+    socket.on("disconnect", () => {
         UserModel.findByIdAndUpdate(
             userId,
             { $set: { online: false } },
             err => {
-                if(err) console.error(err)
-                sendOnlineUsers()
+                if (err) console.error(err);
+                sendOnlineUsers();
             }
-        )
-    })
+        );
+    });
 
     function sendOnlineUsers() {
-        UserModel
-            .find({ online: true }, (err, users) => {
-                if(err) console.error(err)
-                console.log('online: ', users)
-                io.emit('online users', users)
-            })
+        UserModel.find({ online: true }, (err, users) => {
+            if (err) console.error(err);
+            io.emit("online users", users);
+        });
     }
-}
+
+    function saveMessage(packet) {
+        MessageModel.create(packet, err => {
+            if (err) console.err(err);
+        });
+    }
+
+    function sendMessageHistory() {
+        console.log("Retrieving message history...");
+        MessageModel.find({}, (err, messages) => {
+            if (err) console.error(err);
+            console.log("Retrieved messages", messages);
+            io.emit("message history", messages);
+        });
+    }
+};
