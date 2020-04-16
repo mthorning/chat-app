c15209b2d149b7d7a098a98f17a15d561dd4fdaf
Matt Thorning <matthewthorning@gmail.com>
2020-04-16 22:10:43 +0100

    fix state and margin

--- chatbox/components/Online.jsx
+++ chatbox/components/Online.jsx
@@ -1,44 +1,43 @@
-import React, { useEffect, useState, useContext } from 'react'
-import { SocketContext, UserContext } from 'contexts'
-import { MdKeyboardArrowLeft, MdKeyboardArrowRight  } from "react-icons/md";
+import React, { useEffect, useState, useContext } from "react";
+import { SocketContext, UserContext } from "contexts";
+import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
 
 function Online() {
-    const { id } = useContext(UserContext)
-    const socket = useContext(SocketContext)
-    const [open, setOpen] = useState(true);
-    const [onlineUsers, setOnlineUsers] = useState([{ id: 1, name: 'Taya' }]);
+  const { id } = useContext(UserContext);
+  const socket = useContext(SocketContext);
+  const [open, setOpen] = useState(false);
+  const [onlineUsers, setOnlineUsers] = useState([]);
 
-    useEffect(() => {
-        socket.on('online users', onlineUsers => {
-            setOnlineUsers(onlineUsers.filter(user => user._id !== id))
-        })
-    }, [])
+  useEffect(() => {
+    socket.on("online users", (onlineUsers) => {
+      setOnlineUsers(onlineUsers.filter((user) => user._id !== id));
+    });
+  }, []);
 
-    const onClick = () => setOpen(o => !o);
+  const onClick = () => setOpen((o) => !o);
 
-    return (
-        <div className={`online-component ${open ? 'open' : 'closed'}`}>
-            {open 
-                ? (
-                    <>
-                        <MdKeyboardArrowLeft onClick={onClick} className="arrow" /> 
-                        <ul>
-                            {onlineUsers.map(user => (
-                                <li key={user._id}>{user.displayName}</li>
-                            ))}
-                        </ul>
-                    </>
-                )
-                : <MdKeyboardArrowRight onClick={onClick} className="arrow" />
-            }
-        </div>
-    )
+  return (
+    <div className={`online-component ${open ? "open" : "closed"}`}>
+      {open ? (
+        <>
+          <MdKeyboardArrowLeft onClick={onClick} className="arrow" />
+          <ul>
+            {onlineUsers.map((user) => (
+              <li key={user._id}>{user.displayName}</li>
+            ))}
+          </ul>
+        </>
+      ) : (
+        <MdKeyboardArrowRight onClick={onClick} className="arrow" />
+      )}
+    </div>
+  );
 }
 
-export default Online
+export default Online;
 
 const styles = {
-    wrapper: {
-        width: '100px'
-    }
-}
+  wrapper: {
+    width: "100px",
+  },
+};
