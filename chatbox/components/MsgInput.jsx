5afa6da14ac516866d26f5503a05ad6c2fbafe10
Matt Thorning <matthewthorning@gmail.com>
2020-04-18 14:11:43 +0100

    changed scrollbar color

--- chatbox/components/MsgInput.jsx
+++ chatbox/components/MsgInput.jsx
@@ -1,54 +1,51 @@
-import React, { useState, useContext } from 'react'
-import { UserContext, SocketContext } from 'contexts'
+import React, { useState, useContext } from "react";
+import { UserContext, SocketContext } from "contexts";
 
-const adultSymbol = '#'
+const adultSymbol = "#";
 
 function MsgInput() {
-    const [msg, setMsg] = useState('')
-    const { id, displayName, adult } = useContext(UserContext)
-    const socket = useContext(SocketContext)
+  const [msg, setMsg] = useState("");
+  const { id, displayName, adult } = useContext(UserContext);
+  const socket = useContext(SocketContext);
 
-    function inputHandler(e) {
-        if (e.which === 13) {
-            e.preventDefault()
-            const [msgType, message] = newMessage(e.target.value)
-            const timestamp = Date.now()
-            if(message.length) {
-                socket.emit(msgType, {
-                    message,
-                    timestamp,
-                    id,
-                    displayName
-                })
-                setMsg('')
-            }
-        }
+  function inputHandler(e) {
+    if (e.which === 13) {
+      e.preventDefault();
+      const [msgType, message] = newMessage(e.target.value);
+      const timestamp = Date.now();
+      if (message.length) {
+        socket.emit(msgType, {
+          message,
+          timestamp,
+          id,
+          displayName,
+        });
+        setMsg("");
+      }
     }
+  }
 
-    function newMessage(message) {
-        if(adult && message.charAt(0) === adultSymbol) {
-            return [
-                'adult message',
-                message.substring(1).trim()
-            ]
-        }
-        return ['chat message', message.trim()]
+  function newMessage(message) {
+    if (adult && message.charAt(0) === adultSymbol) {
+      return ["adult message", message.substring(1).trim()];
     }
+    return ["chat message", message.trim()];
+  }
 
-    function changeHandler(e) {
-        setMsg(e.target.value)
-    }
+  function changeHandler(e) {
+    setMsg(e.target.value);
+  }
 
-    return (
-        <div className="msg-input-component">
-            <textarea
-                autoFocus
-                value={msg}
-                onChange={changeHandler}
-                onKeyPress={inputHandler}
-            />
-        </div>
-    )
+  return (
+    <div className="msg-input-component">
+      <textarea
+        autoFocus
+        value={msg}
+        onChange={changeHandler}
+        onKeyPress={inputHandler}
+      />
+    </div>
+  );
 }
 
-export default MsgInput
+export default MsgInput;
